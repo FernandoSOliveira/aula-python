e1c0aaa1ecc8eeaeafd072e3331389dd4640ac14
Fernando_Oliveira <fernandosantos.oliveira@outlook.com>
2025-04-17 10:35:59 -0300

    atribuiçao de variaveis

---
+++ aula5.py
@@ -0,0 +1,10 @@
+#atribuir variaveis
+nome_completo = "Fernando Santos"
+print(nome_completo)
+
+nome_completo = "Fernando Santos"
+idade = 36 
+maior_idade = idade >= 18
+print("nome_completo:", nome_completo)
+print("idade:", idade)
+print("maior_idade?", maior_idade) 
